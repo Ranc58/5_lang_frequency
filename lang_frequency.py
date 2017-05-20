827c997d64836a3556caf053d915740be65e89c7
Ranc58 <rancvova@gmail.com>
2017-05-20 15:16:09 +0300

    second version

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,20 +1,22 @@
+import os
 import re
 from collections import Counter
 
 
 def load_data(filepath):
-    try:
-        with open(filepath, 'r', encoding='UTF-8') as text:
+    with open(filepath, 'r', encoding='UTF-8') as text:
             return text.read()
-    except Exception:
-        print('Error! Please check way to file and load correct file!')
 
 
 def get_most_frequent_words(text):
     found_words = re.findall(r'\b\w+', text)
-    quantity_of_words = Counter(found_words).most_common(10)
+    quantity_of_duplicate_words = Counter(found_words).most_common(10)
+    return quantity_of_duplicate_words
+
+
+def output_duplicate_words(quantity_of_duplicate_words):
     print('List of repeated words: ')
-    for word_repeats in quantity_of_words:
+    for word_repeats in quantity_of_duplicate_words:
         word = word_repeats[0]
         repeat_quantity = word_repeats[1]
         print("Word '%s' repeated %d times." % (word, repeat_quantity))
@@ -22,6 +24,8 @@ def get_most_frequent_words(text):
 
 if __name__ == '__main__':
     filepath = input('Please enter way to txt file: ')
-    text = load_data(filepath)
-    if text:
-        get_most_frequent_words(text)
+    if os.path.exists(filepath):
+        text = load_data(filepath)
+        output_duplicate_words(get_most_frequent_words(text))
+    else:
+        print('File not exist! Check way to file.')
